95d2f21d9115dcc50709b72def85a882f4dff8d0
HarshitGupta713 <guptaharshit713@gmail.com>
2023-12-11 22:51:22 +0530

    created the project and add the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ExhibitComponent } from './exhibit/exhibit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: 'exhibit/:id', component: ExhibitComponent },
+];
---
+++ src/app/exhibit/exhibit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibit',
+  standalone: true,
+  imports: [],
+  templateUrl: './exhibit.component.html',
+  styleUrl: './exhibit.component.scss'
+})
+export class ExhibitComponent {
+
+}
